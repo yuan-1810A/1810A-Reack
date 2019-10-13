1f1c02f15cfc94cb78586528d9fceaeb4713fd4a
MayGodBlessYouQi <2489919598@qq.com>
2019-10-13 11:34:32 +0800

    o98k

--- src/10-12天猫作业/fixed/index.jsx
+++ src/10-12天猫作业/fixed/index.jsx
@@ -1,53 +1,48 @@
 import React from 'react';
 import './index.css';
 const fixedDate = [
-   { id:'1',lable:'综合 ↓',className:'fixed_text red'},
-   { id:'2',lable:'人气 ↓',className:'fixed_text'},
-   { id:'3',lable:'新品 ↓',className:'fixed_text'},
-   { id:'4',lable:'销量 ↓',className:'fixed_text'},
-   { id:'5',lable:'价格 ^',className:'fixed_text'}
+    { id: '1', lable: '综合 ↓', className: 'fixed_text red' },
+    { id: '2', lable: '人气 ↓', className: 'fixed_text' },
+    { id: '3', lable: '新品 ↓', className: 'fixed_text' },
+    { id: '4', lable: '销量 ↓', className: 'fixed_text' },
+    { id: '5', lable: '价格 ^', className: 'fixed_text' }
 ]
-export default class Index extends React.Component{
-    render(){
-        return( 
+export default class Index extends React.Component {
+    render() {
+        return (
             <React.Fragment>
-                <p>11</p>
-                <p>11</p>
-                <p>11</p>
-                <p>11</p>
-                <p>11</p>
-        <div className='fixed'>
-            {fixedDate.map((items,index)=>{
-                return <div className={items.className} key={items.id}>{items.lable}</div>
-            })}
-             <div className='site'>
-        <div className='site_left'>收获地:</div>
-         <div className='site_right'>杭州</div>
-          </div>
-         <div className='site br'>
-             <div className='site_text'>￥请输入</div>
-             <div className='drop'>-</div>
-             <div className='site_text'>￥请输入</div>
-         </div>
-          <div className='fM_con'>
-          <label htmlFor="" className='new'><input className='inp' type="checkbox"/>新到商品</label>
-          <label htmlFor=""><input className='inp' type="checkbox"/>包邮</label>
-          <label htmlFor=""><input className='inp' type="checkbox"/>折扣</label>
-          <div className='more'>更多<div className='i'>></div></div>
-          </div>
-          <div className='fType'>
-              <div className='fType_w'><i className='icon_black'></i>店铺</div>
-              <div className='fType_w red'><i className='icon_red'></i>大图</div>
-          </div>
-          <div className='right'>
-               1/80 
+                <div className='fixed'>
+                    {fixedDate.map((items, index) => {
+                        return <div className={items.className} key={items.id}>{items.lable}</div>
+                    })}
+                    <div className='site'>
+                        <div className='site_left'>收获地:</div>
+                        <div className='site_right'>杭州</div>
+                    </div>
+                    <div className='site br'>
+                        <div className='site_text'>￥请输入</div>
+                        <div className='drop'>-</div>
+                        <div className='site_text'>￥请输入</div>
+                    </div>
+                    <div className='fM_con'>
+                        <label htmlFor="" className='new'><input className='inp' type="checkbox" />新到商品</label>
+                        <label htmlFor=""><input className='inp' type="checkbox" />包邮</label>
+                        <label htmlFor=""><input className='inp' type="checkbox" />折扣</label>
+                        <div className='more'>更多<div className='i'>></div></div>
+                    </div>
+                    <div className='fType'>
+                        <div className='fType_w'><i className='icon_black'></i>店铺</div>
+                        <div className='fType_w red'><i className='icon_red'></i>大图</div>
+                    </div>
+                    <div className='right'>
+                        1/80
              <div className='right_1 gray'> ^ </div>
-              <div  className='right_1 fff'> > </div>
-          </div>
-        </div>
-        <p style={{height:'2000px'}}>11</p>
-              
-        </React.Fragment>
+                        <div className='right_1 fff'> > </div>
+                    </div>
+                </div>
+                <p style={{ height: '2000px' }}>11</p>
+
+            </React.Fragment>
         )
     }
 }
--- src/10-12天猫作业/header/header.jsx
+++ src/10-12天猫作业/header/header.jsx
@@ -1,7 +1,39 @@
 import React from 'react'
+// 引入头部的css
+import './css/head.css'
 
 export default () => {
-	return <div>
-		我是div
+	return <div className="header">
+		<div className="top-con-top flex">
+			<ul className="header-top-left flex">
+				<li>
+					<i className="header-top-fang"></i>
+					<a href="https://www.tmall.com/">天猫首页</a>
+				</li>
+				<li>
+					喵，欢迎来到天猫
+				</li>
+				<li>
+					<a href="https://login.tmall.com/?redirectURL=https%3A%2F%2Flist.tmall.com%2Fsearch_product.htm%3Fq%3D%25CA%25D6%25BB%25FA%26type%3Dp%26vmarket%3D%26spm%3D875.7931836%252FB.a2227oh.d100%26from%3Dmallfp..pc_1_searchbutton">请登录</a>
+				</li>
+				<li>
+					<a href="https://register.tmall.com/">免费注册</a>
+				</li>
+			</ul>
+			<ul className="header-top-right flex">
+				<li>
+					<a href="https://login.taobao.com/member/login.jhtml?from=taobaoindex&f=top&style=&sub=true&redirect_url=https%3A%2F%2Fi.taobao.com%2Fmy_taobao.htm">我的淘宝</a>
+				</li>
+				<li>
+					<i></i>
+					<a href="">购物车<span>0</span>件</a>
+				</li>
+				<li></li>
+				<li></li>
+				<li></li>
+				<li></li>
+				<li></li>
+			</ul>
+		</div>
 	</div>
 }
